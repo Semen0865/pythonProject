4e85fa3cc90a8874e2f61fbbb6a321c4de73f848
XIAOMI <minecraft08072011@gmail.com>
2024-03-24 18:01:46 +0200

    first commit DZ

---
+++ Lesson 4.py
@@ -0,0 +1,24 @@
+import random
+class Human:
+    def __init__(self, name="Human", job=None, home=None, car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
+        def get_home(self):
+            self.home = House()
+
+
+        def get_car(self):
+            self.car = Auto(brands_of_car)
+        def get_job(self):
+            if self.car.drive():
+                pass
+            else:
+                self.to_repair()
+                return
+            self.job = Job(job_list)
+
